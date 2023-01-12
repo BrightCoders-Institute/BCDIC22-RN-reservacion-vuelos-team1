3945ea9738eb3ba0c51f1d61e329c9d0f742da88
Gaddiel Patiño <alegaddiel@gmail.com>
2023-01-12 16:54:39 -0600

    creacion de proyecto aerolinea carrillo

---
+++ firebaseconfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbl988i9Q7jTxlYT5spcQkuPeBPyL51f0",
+  authDomain: "reservacion-vuelos-76a75.firebaseapp.com",
+  projectId: "reservacion-vuelos-76a75",
+  storageBucket: "reservacion-vuelos-76a75.appspot.com",
+  messagingSenderId: "201005805305",
+  appId: "1:201005805305:web:351bca4a4b9ecb1003bc28",
+  measurementId: "G-N7H1YKL64M"
+};

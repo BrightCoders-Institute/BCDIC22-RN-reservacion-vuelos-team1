a485cd100f03c064ee7bdb3748332f131f218d99
GERARDO ISMAEL GARCIA GONZALEZ <ggarcia26@ucol.mx>
2023-01-26 18:45:39 -0600

    Verificacion de la cuenta de este o no creada en firebase
    Co-authored-by: Gaddiel Patiño <GaddPat0609@users.noreply.github.com>
    Co-authored-by: EliasCode79 <EliasCode79@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { View } from 'react-native'
-import PantallaLog from './src/views/pantallaLog/pantallaLog'
 import PantallaRegis from './src/views/pantallaRegis/pantallaRegis'
 import Constants from 'expo-constants'
 import PantallaOrigen from './src/views/pantallaOriDes/pantallaOrigen'
 import PantallaOriDes from './src/views/pantallaOriDes/pantallaDestino'
 import PantallaCalendario from './src/views/pantallaOriDes/pantallaCalendario'
 import PantallaPasajeros from './src/views/pantallaOriDes/pantallaPasajeros'
-
+import PantallaLogin from './src/views/pantallaLogin/pantallaLogin'
 
 export default function App() {
   return (
     <View style={{ marginTop: Constants.statusBarHeight }}>
-      {/* <PantallaLog /> */}
       {/* <PantallaRegis /> */}
+      <PantallaLogin />
       {/* <PantallaOrigen /> */}
       {/* <PantallaOriDes /> */}
-      {/* <PantallaCalendario /> */}
-      <PantallaPasajeros />
-
-
+      {/*<PantallaCalendario />*/}
+      {/*  <PantallaPasajeros />*/}
     </View>
   )
 }
--- src/views/pantallaRegis/styles.js
+++ src/views/pantallaRegis/styles.js
@@ -6,45 +6,77 @@ export default StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  /* Textos de la pantalla de registro*/
+  /* Textos de la la primera pantalla*/
   TextoSing: {
     color: 'blue',
-    fontSize: 30,
-    marginLeft: 20,
-    marginTop: 70,
-  },
-  Textocorreo: {
-    color: 'grey',
-    fontSize: 19,
-    marginLeft: 20,
-    marginTop: 100,
-  },
-  TextoPass: {
-    color: 'grey',
-    fontSize: 20,
+    fontSize: 25,
     marginLeft: 20,
     marginTop: 50,
   },
-  Textobajo: {
+  Textoname: {
     color: 'grey',
     fontSize: 18,
     marginLeft: 20,
-    marginTop: 30,
+    marginTop: 40,
+  },
+  Textocorreo: {
+    color: 'grey',
+    fontSize: 18,
+    marginLeft: 20,
+    marginTop: 40,
+  },
+  TextoPass: {
+    color: 'grey',
+    fontSize: 18,
+    marginLeft: 20,
+    marginTop: 40,
+  },
+  Textobajo: {
+    color: 'grey',
+    fontSize: 15.5,
+    marginLeft: 20,
+    marginTop: 20,
+  },
+  TextoCheck: {
+    color: 'grey',
+    fontFamily: 'Arial',
+    fontSize: 17.5,
+    textAlignVertical: 'center',
+    fontWeight: '100',
   },
   TextoOr: {
     color: 'grey',
+    fontFamily: 'Arial',
     fontSize: 20,
     marginLeft: 118,
-    marginTop: 25,
+    marginTop: 10,
+  },
+  TextoFuter: {
+    color: 'grey',
+    fontFamily: 'Arial',
+    fontSize: 17.5,
+    marginTop: 15,
+    marginLeft: 90,
   },
 
+  /* componentes react n paper*/
+  PrimerCheck: {
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  SegundoCheck: {
+    marginLeft: 10,
+    marginTop: 5,
+    flexDirection: 'row',
+  },
   /* componentes Boton*/
   primerBoton: {
-    marginTop: 30,
+    marginTop: 15,
     alignItems: 'center',
   },
   segundoBoton: {
-    marginTop: 30,
+    marginTop: 15,
     alignItems: 'center',
   },
 })

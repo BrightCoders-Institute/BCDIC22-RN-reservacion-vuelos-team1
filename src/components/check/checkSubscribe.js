bd7d022cab4e628ef101d6b373ad417ed1366279
Gaddiel Patiño <alegaddiel@gmail.com>
2023-02-03 16:25:45 -0600

    Se realizaron las validaciones de los campos del registro de usuarios.

--- src/components/check/checkSubscribe.js
+++ src/components/check/checkSubscribe.js
@@ -3,8 +3,6 @@ import { Checkbox } from 'react-native-paper'
 import styles from '../cajaTextoVisible/styles'
 
 export const CheckSubscribe = ({checkout}) => {
-  // const [checked, setChecked] = React.useState(false)
-  // console.log(user)
   return (
     <Checkbox
       status={checkout ? 'checked' : 'unchecked'}
